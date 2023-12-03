1adf6f3226901f66d8810f933f0b52269677364b
keimschleuder <freunde@niklas-keim.de>
2023-12-03 20:29:20 +0100

    Removed Colors from Lock screen
    Farben aus dem Sperrbildschirm Widget entfernt, weil diese unnötig sind

--- Sperrbildschirm Kalender.js
+++ Sperrbildschirm Kalender.js
@@ -9,23 +9,6 @@ const VISIBLE_CALENDARS = ["Privat", "Schule", "Freizeit", "Kirche", "Orchester"
 const NUM_ITEMS_TO_SHOW = 1 // 1 is the max without it being cramped
 const NO_ITEMS_MESSAGE = "Enjoy it." // what's displayed when you have no items for the day
 
-const DATE_COLOR = Color.red()
-const ITEM_NAME_COLOR = Color.white()
-const ITEM_TIME_COLOR = new Color("#eeeeee")
-
-const CALENDAR_COLORS = {
-    "Privat": Color.blue(),
-    "Freizeit": Color.blue(),
-    "Kirche": new Color("#e67c73"), // flamingo
-    "Orchester": Color.yellow(),
-    "Schule": Color.red(),
-    "Family": Color.purple(),
-    "Spezial": Color.orange(),
-    "Deutsche Feiertage": Color.green(),
-    "Geburtstage": Color.cyan()
-}
-
-const DATE_SIZE = 16
 const ITEM_NAME_SIZE = 14
 const ITEM_TIME_SIZE = 12
 
@@ -68,18 +51,15 @@ if (!config.runsInWidget && !TEST_MODE) {
     // If there is at least one item today
 
     if (itemsToShow.length > 0) {
-        let itemName = widget.addText(formatItemName(item))
+        let itemName = widget.addText(item.name)
         itemName.font = Font.semiboldSystemFont(ITEM_NAME_SIZE)
-        itemName.textColor = ITEM_NAME_COLOR
         widget.addSpacer(5)
             
         let itemDate = widget.addText(formatItemDate(item))
         itemDate.font = Font.mediumSystemFont(ITEM_TIME_SIZE)
-        itemDate.textColor = getItemColor(item)
     } else { // If there are no more items today
         // No events found:
         let message = widget.addText(NO_ITEMS_MESSAGE)
-        message.textColor = ITEM_NAME_COLOR
         message.font = Font.lightSystemFont(ITEM_NAME_SIZE)  
     }
 
@@ -125,22 +105,6 @@ function formatItemDate(item) {
     }
 }
 
-function getItemColor(item) {
-    if (item.isReminder === true) {
-        return REMINDER_COLORS[item.calendarTitle]
-    } else {
-        return CALENDAR_COLORS[item.calendarTitle]
-    }
-}
-
-function formatItemName(item) {
-    return item.name
-}
-
 function getItemUrl(item) {
-    if (item.isReminder === false) {
-        return CALENDAR_URL + item.id
-    } else {
-        return REMINDERS_URL + item.id
-    }
+    return CALENDAR_URL + item.id  
 }
